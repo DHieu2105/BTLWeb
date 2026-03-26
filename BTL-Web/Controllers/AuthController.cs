863884318fcb3ae003f6dcf8e03532b5b0f36557
DHieu <hyoyoeng2105@gmail.com>
2026-03-26 19:57:59 +0700

    init project

---
+++ BTL-Web/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+public class AuthController : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Login(string username, string password)
+    {
+        if (username == "admin" && password == "123")
+        {
+            HttpContext.Session.SetString("user", username);
+            return RedirectToAction("Index", "Home");
+        }
+
+        ViewBag.Error = "Sai tài khoản";
+        return View();
+    }
+
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Login");
+    }
+}
