08e60bc9eb79a2e87325834cd9684525c533694e
Minh Huy Mai <minhhhhhxt123@gmail.com>
2026-04-09 12:10:15 +0700

    feat: hoàn thành CRUD học viên và dọn dẹp gitignore

---
+++ BTL-Web/Controllers/HocViensController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BTL_Web.Models;
+
+namespace BTL_Web.Controllers
+{
+    public class HocViensController : Controller
+    {
+        private readonly TtanContext _context;
+
+        public HocViensController(TtanContext context)
+        {
+            _context = context;
+        }
+
+        // GET: HocViens
+        public async Task<IActionResult> Index()
+        {
+            var ttanContext = _context.HocViens.Include(h => h.MaNvNavigation);
+            return View(await ttanContext.ToListAsync());
+        }
+
+        // GET: HocViens/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hocVien = await _context.HocViens
+                .Include(h => h.MaNvNavigation)
+                .FirstOrDefaultAsync(m => m.MaHocVien == id);
+            if (hocVien == null)
+            {
+                return NotFound();
+            }
+
+            return View(hocVien);
+        }
+
+        // GET: HocViens/Create
+        public IActionResult Create()
+        {
+            ViewData["MaNv"] = new SelectList(_context.NhanViens, "MaNv", "MaNv");
+            return View();
+        }
+
+        // POST: HocViens/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaHocVien,HoVaTen,Sdt,NgayDangKi,MaNv,GioiTinh")] HocVien hocVien)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(hocVien);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaNv"] = new SelectList(_context.NhanViens, "MaNv", "MaNv", hocVien.MaNv);
+            return View(hocVien);
+        }
+
+        // GET: HocViens/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hocVien = await _context.HocViens.FindAsync(id);
+            if (hocVien == null)
+            {
+                return NotFound();
+            }
+            ViewData["MaNv"] = new SelectList(_context.NhanViens, "MaNv", "MaNv", hocVien.MaNv);
+            return View(hocVien);
+        }
+
+        // POST: HocViens/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("MaHocVien,HoVaTen,Sdt,NgayDangKi,MaNv,GioiTinh")] HocVien hocVien)
+        {
+            if (id != hocVien.MaHocVien)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(hocVien);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HocVienExists(hocVien.MaHocVien))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaNv"] = new SelectList(_context.NhanViens, "MaNv", "MaNv", hocVien.MaNv);
+            return View(hocVien);
+        }
+
+        // GET: HocViens/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hocVien = await _context.HocViens
+                .Include(h => h.MaNvNavigation)
+                .FirstOrDefaultAsync(m => m.MaHocVien == id);
+            if (hocVien == null)
+            {
+                return NotFound();
+            }
+
+            return View(hocVien);
+        }
+
+        // POST: HocViens/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var hocVien = await _context.HocViens.FindAsync(id);
+            if (hocVien != null)
+            {
+                _context.HocViens.Remove(hocVien);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HocVienExists(string id)
+        {
+            return _context.HocViens.Any(e => e.MaHocVien == id);
+        }
+    }
+}
--- BTL-Web/Models/KetQua.cs
+++ BTL-Web/Models/KetQua.cs
@@ -1,21 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; 
 
 namespace BTL_Web.Models;
 
 public partial class KetQua
 {
+    [Display(Name = "Mã học viên")]
     public string MaHocVien { get; set; } = null!;
 
+    [Display(Name = "Mã khóa học")]
     public string MaKhoaHoc { get; set; } = null!;
 
+    [Display(Name = "Điểm Nghe")]
+    [Range(0, 495, ErrorMessage = "Điểm Listening phải từ 0 đến 495")] // Quy tắc TOEIC
     public int? DiemListening { get; set; }
 
+    [Display(Name = "Điểm Đọc")]
+    [Range(0, 495, ErrorMessage = "Điểm Reading phải từ 0 đến 495")] // Quy tắc TOEIC
     public int? DiemReading { get; set; }
 
+    [Display(Name = "Tổng điểm")]
+    [Range(0, 990, ErrorMessage = "Tổng điểm không hợp lệ")]
     public int? DiemTong { get; set; }
 
+    // Các phần quan hệ này Minh giữ nguyên nhé
     public virtual HocVien MaHocVienNavigation { get; set; } = null!;
-
     public virtual KhoaHoc MaKhoaHocNavigation { get; set; } = null!;
 }
